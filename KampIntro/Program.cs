40ac998739e72d808a405c79c07ef2bb4d0df0b1
Uğur Heper <kerimugur62@outlook.com>
2021-01-16 23:54:29 +0300

    16 Ocak ders kaydı

---
+++ KampIntro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 3200;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double DolarDun = 7.45;
+            double DolarBugun = 7.45;
+
+            if (DolarDun > DolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (DolarDun < DolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi butonu");
+            }
+            if (sistemeGirisYapmisMi == true )
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
