68c812c208ce00a286fcdc6ea4da781441543dd2
Uğur Heper <kerimugur62@outlook.com>
2021-01-22 22:10:02 +0300

    Ödev5

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] numbers;
+        
+
+        public MyDictionary()
+        {
+            numbers = new T[0];
+            
+        }
+
+        public void Add(T number)
+        {
+            T[] tempArray = numbers;
+            
+
+            numbers = new T[numbers.Length+1];
+            
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                numbers[i] = tempArray[i];
+                
+            }
+
+            numbers[numbers.Length - 1] = number;
+            
+        }
+
+        public int Lenght
+        {
+            get { return numbers.Length; }
+        }
+
+        public T[] Numbers
+        {
+            get { return numbers; }
+        }
+
+      
+
+    }
+
+
+    
+
+    
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> sayilar = new MyDictionary<int>();
+            sayilar.Add(35);
+            sayilar.Add(39);
+
+            Console.WriteLine(sayilar.Lenght);
+
+            foreach (var sayi in sayilar.Numbers )
+            {
+                Console.WriteLine(sayi);
+            }
+
+            
+        }
+    }
+}
